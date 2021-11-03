ef7fad4193a3bd10cc3e12e38a1617bbdc12a3e4
TonyNavas <tonynavas777@gmail.com>
2021-11-02 22:33:10 -0600

    Valide los campos en el modelo de usuario

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Usuarios extends Model
 {
     use HasFactory;
+    protected $table='usuarios';
+    protected $fillable=[
+        'nombre',
+        'email',
+        'password',
+    ];
 }
