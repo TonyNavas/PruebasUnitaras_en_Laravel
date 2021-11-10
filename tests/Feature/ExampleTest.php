b3dfb69a35616063511460d7e1fed11c7e1f828f
TonyNavas <tonynavas777@gmail.com>
2021-11-09 21:35:38 -0600

    Agregue las operaciones de crud faltantes: store, index y show

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,24 +18,57 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+    public function test_update()
+    {
+        $response = $this->put('/api/usuarios/7', 
+        ['nombre' => 'cesar',
+         'email' => 'cesar@gmail.com',
+         'password' => '12345678']);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'mensaje' => 'Usuario actualizado exitosamente',
+            ]);
+    }
 
     public function test_delete()
     {
-        $response = $this->delete('/api/usuarios/4');
+        $response = $this->delete('/api/usuarios/3');
         $response
             ->assertStatus(200)
             ->assertJson([
                 'mensaje' => 'Usuario eliminado exitosamente',
             ]);
     }
-    public function test_update()
+
+    public function test_show()
     {
-        $response = $this->put('/api/usuarios/3', ['nombre' => 'juan','email' => 'cesar@gmail.com','password' => '12345678']);
+        $response = $this->get('/api/usuarios/3');
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+            ]);
+    }
+    public function test_index()
+    {
+        $response = $this->get('/api/usuarios');
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+            ]);
+    }
+    public function test_store()
+    {
+        $response = $this->post('/api/usuarios', 
+        ['nombre' => 'tony',
+         'email' => 'tony@gmail.com',
+         'password' => '12345678']);
 
         $response
             ->assertStatus(200)
             ->assertJson([
-                'mensaje' => 'Usuario actualizado exitosamente',
+                'mensaje' => 'Usuario guardado exitosamente',
             ]);
     }
 }
